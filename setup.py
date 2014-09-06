7cc6f0767b96136099a8bb73298b4ccc4e4cb355
Kiss György <kissgyorgy@me.com>
2014-09-06 02:19:49 +0200

    Removed unneccesary setuptools test command. Also not working perfectly.

--- setup.py
+++ setup.py
@@ -2,25 +2,6 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
 setup(
     name = 'enum34-custom',
     version = '0.6.0',
@@ -41,5 +22,4 @@ setup(
     license = 'MIT',
     py_modules = ['enum34_custom'],
     tests_require = ['pytest', 'pytest-raisesregexp'],
-    cmdclass = {'test': PyTest},
 )
