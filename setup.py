fcd781839201e783c2d46d53a09ab6b1e5291533
Kiss György <kissgyorgy@me.com>
2014-09-26 16:30:02 +0200

    Using six for compatibility. Python 2 classifiers for PYPI.

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
+import six
+
+
+if six.PY2:
+    install_requires = ['six', 'enum34']
+else:
+    install_requires = ['six']
 
 
 setup(
@@ -13,6 +20,8 @@ setup(
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
     ],
@@ -22,5 +31,6 @@ setup(
     url = 'https://github.com/Walkman/enum34-custom',
     license = 'MIT',
     py_modules = ['enum34_custom'],
+    install_requires = install_requires,
     tests_require = ['pytest', 'pytest-raisesregexp'],
 )
